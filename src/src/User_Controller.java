93f5aff41fbda3eece6223d646135f04e8652e5a
Jairun Diemert <jsd25@students.uwf.edu>
2019-10-23 10:19:17 -0500

    Adding User_Controller.java to support View buttons

---
+++ src/src/User_Controller.java
@@ -0,0 +1,32 @@
+
+public class User_Controller {
+
+	void purchaseTicket() {};
+	
+	void exitUserInterface() {};
+	
+	void createNewUser() {};
+	
+	void inputNewUserInfo() {};
+	
+	void pushNewUserInfoToDatabase() {};
+	
+	void viewExistingUser() {};
+	
+	void getUserByUserName() {};
+	
+	void retreiveUserInfoFromDatabase() {};
+	
+	void formatUserInfo() {};
+	
+	void returnUserInfo() {};
+	
+	void selectParkingPermitType() {};
+	
+	void slelctParkingPermitPeriod() {};
+	
+	void registerVehicleByTag() {};
+	
+	void submitNewPermitInfo() {};
+	
+}
