3dd002ecc9b93f1ea5d923764ce690d19d21ea60
GaryMarmon <glm19@students.uwf.edu>
2019-10-25 11:37:24 -0500

    updated cal unit test

--- Test/testing/Permit_Cost_CalculationTest.java
+++ Test/testing/Permit_Cost_CalculationTest.java
@@ -3,32 +3,21 @@ package testing;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import models.User_Info;
+import models.permit;
 import src.Permit_Cost_Calculation;
 
 public class Permit_Cost_CalculationTest {
 
 	@Test
-	public void testDiscountCal() {
+	public void testCal() {
+		User_Info user = new User_Info();
+		permit userPermit = new permit();
 		Permit_Cost_Calculation calculator = new Permit_Cost_Calculation();
-		assertEquals(0.30, calculator.discountCal("Veteran"), 0);
-		assertEquals(0.30, calculator.discountCal("Employee"), 0);
-		assertEquals(1, calculator.discountCal("Student"), 0);
+		user.setUserInfo("Student");
+		user.setUserLot("A");
+		userPermit.setStartHour(14);
+		userPermit.setEndHour(16);
+		assertEquals("The cost is ", calculator.cal(user, userPermit));
 	}
-
-	@Test
-	public void testLotCal() {
-		Permit_Cost_Calculation calculator = new Permit_Cost_Calculation();
-		assertEquals(1.5, calculator.lotCal("A"), 0);
-		assertEquals(1.1, calculator.lotCal("B"), 0);
-		assertEquals(0.9, calculator.lotCal("C"), 0);
-		assertEquals(1.3, calculator.lotCal("D"), 0);
-	}
-
-	@Test
-	public void testRateCal() {
-		//Permit_Cost_Calculation calculator = new Permit_Cost_Calculation();
-		//not sure what to pass here
-		//assertEquals(0, calculator.rateCal(""), 0);
-	}
-
 }
